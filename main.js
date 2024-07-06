f9ca49f272a38e4466c58ec3166be9afd7a493c0
Naimul Alam <58821549+Naimul0307@users.noreply.github.com>
2024-07-06 14:56:21 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,43 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const url = require('url');
 
 function createWindow() {
-    mainWindow = new BrowserWindow({
-      width: 800,
-      height: 600,
-      fullscreen: true, // Start in full-screen mode
-      autoHideMenuBar: true, // Hide the menu bar
-      icon: path.join(__dirname, 'icon.ico'), // Add this line for the i
-      webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false,
-      }
-    });
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: true, // Start in full-screen mode
+    autoHideMenuBar: true, // Hide the menu bar
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    }
+  });
 
-    mainWindow.loadFile('public/index.html');
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'templates', 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Remove DevTools opening for production
+  // mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    mainWindow = null;
+  });
 }
 
 app.on('ready', createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
